78239884d050f60374a06b44c63f820a9f3d185b
Ramiro Noronha <ramironrribeiro@gmail.com>
2023-01-21 13:10:21 -0300

    Implementação das Exceptions

---
+++ Dart - Lidando com Exception e Null Safaty/dart_application__module_4/bin/dart_application__module_4.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application__module_4/dart_application__module_4.dart' as dart_application__module_4;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application__module_4.calculate()}!');
+}
---
+++ Dart - Lidando com Exception e Null Safaty/dart_exceptions-86d2deb91f75d01f8532983a0f7eb93b231b5238/bin/exceptions/bank_controller_exceptions.dart
@@ -0,0 +1,1 @@
+class SendIdNotInvalidException implements Exception {}
