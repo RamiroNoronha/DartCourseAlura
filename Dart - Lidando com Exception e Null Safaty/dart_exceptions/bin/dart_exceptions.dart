dda730ac911318bab3ab07c4fbcc5f370cf42c28
Ramiro Noronha <ramironrribeiro@gmail.com>
2023-01-21 15:10:17 -0300

    Criando os atributos das exceções e tratando elas na main

--- Dart - Lidando com Exception e Null Safaty/dart_exceptions/bin/dart_exceptions.dart
+++ Dart - Lidando com Exception e Null Safaty/dart_exceptions/bin/dart_exceptions.dart
@@ -1,4 +1,5 @@
 import 'controllers/bank_controller.dart';
+import 'exceptions/bank_controller_exceptions.dart';
 import 'models/account.dart';
 
 void main() {
@@ -15,11 +16,33 @@ void main() {
       id: "Kako",
       account:
           Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+  try {
+    // Fazendo transferência
+    bool result = bankController.makeTransfer(
+        idSender: "Kako", idReceiver: "Ricarth", amount: 300);
 
-  // Fazendo transferência
-  bool result = bankController.makeTransfer(
-      idSender: "Kako", idReceiver: "Ricarth", amount: 700);
-
-  // Observando resultado
-  print(result);
+    // Observando resultado
+    if (result) print("Transferência realizada com sucesso!");
+  } on SendIdNotInvalidException catch (ex, stackTrace) {
+    print('O id ${ex.senderId} não é válido');
+    print('Pilha de execução: ');
+    print('${stackTrace.toString()}');
+  } on ReceiverIdInvalidException catch (ex, stackTrace) {
+    print('O id ${ex.idReceiver} não é válido');
+    print('Pilha de execução: ');
+    print('${stackTrace.toString()}');
+  } on SenderNotAuthenticatedException catch (ex, stackTrace) {
+    print('A conta com o id ${ex.senderId} não está autenticada');
+    print('Pilha de execução: ');
+    print('${stackTrace.toString()}');
+  } on SenderBalanceLowerThenException catch (ex, stackTrace) {
+    print(
+      'A conta com o id ${ex.senderId} tentou enviar R\$ ${ex.amount} possuindo um saldo de R\$ ${ex.senderBalance}',
+    );
+    print('Pilha de execução: ');
+    print('${stackTrace.toString()}');
+  } catch (ex) {
+    print('Erro Inesperado');
+    print('Tipo do erro ${ex.runtimeType}');
+  }
 }
--- Dart - Lidando com Exception e Null Safaty/dart_exceptions/bin/exceptions/bank_controller_exceptions.dart
+++ Dart - Lidando com Exception e Null Safaty/dart_exceptions/bin/exceptions/bank_controller_exceptions.dart
@@ -1,1 +1,25 @@
-class SendIdNotInvalidException implements Exception {}
+class SendIdNotInvalidException implements Exception {
+  String senderId;
+
+  SendIdNotInvalidException({required this.senderId});
+}
+
+class ReceiverIdInvalidException implements Exception {
+  String idReceiver;
+  ReceiverIdInvalidException({required this.idReceiver});
+}
+
+class SenderNotAuthenticatedException implements Exception {
+  String senderId;
+  SenderNotAuthenticatedException({required this.senderId});
+}
+
+class SenderBalanceLowerThenException implements Exception {
+  String senderId;
+  double senderBalance;
+  double amount;
+  SenderBalanceLowerThenException(
+      {required this.senderId,
+      required this.senderBalance,
+      required this.amount});
+}
