77b4d5165f421af62a6122b45854989bba6edfdd
Ramiro Noronha <ramironrribeiro@gmail.com>
2023-01-21 14:26:21 -0300

    Ajustes nas pastas

--- Dart - Lidando com Exception e Null Safaty/dart_application__module_4/bin/dart_application__module_4.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_application__module_4/dart_application__module_4.dart' as dart_application__module_4;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_application__module_4.calculate()}!');
-}
--- Dart - Lidando com Exception e Null Safaty/dart_application__module_4/test/dart_application__module_4_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_application__module_4/dart_application__module_4.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
